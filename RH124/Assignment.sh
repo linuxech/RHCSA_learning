1d28504f0a8b7bda8b77037ebe08b1dc2fe7459d
Ashish <Ashishsemwal50@gmail.com>
2025-09-11 10:48:09 +0530

    ASsignment progress

--- RH124/Assignment.sh
+++ RH124/Assignment.sh
@@ -13,6 +13,15 @@ Warning: 8
 - Save file. 
 Note this will only impact the new user that will be created now.
 
+// What I have done till now
 
+// Created the two directories
+// Created users 
+// Now, need to create users passwords
+// Need to create secondary groups
+/ need to add harry & Natasha  to projecta group
+/ need to add sarah & thomas to projectx group. 
+
+Need to set the password policy
 
 
